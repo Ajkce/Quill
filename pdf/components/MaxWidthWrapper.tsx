69a595ca5c914161d2e5fc339df2a553c5f0c803
Ajaya Bahadur K C <kshettryaj93@gmail.com>
2023-09-30 15:32:32 +1000

    Initial project and configuration setup for the project

---
+++ pdf/components/MaxWidthWrapper.tsx
@@ -0,0 +1,24 @@
+import { cn } from "@/lib/utils";
+import React, { ReactNode } from "react";
+
+const MaxWidthWrapper = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: ReactNode;
+}) => {
+  return (
+    <div
+      // THe utility funcion that we just created is being used to merge the classname that we provide here and the other one we provide while using the component together
+      className={cn(
+        "mx-auto w-full max-w-screen-xl px-2.5 md:px-20 text-center",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default MaxWidthWrapper;
---
+++ pdf/lib/utils.ts
@@ -0,0 +1,10 @@
+import clsx from "clsx";
+import { ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+
+    //px-2 py-2 => p-2
+
+  return twMerge(clsx(inputs));
+}

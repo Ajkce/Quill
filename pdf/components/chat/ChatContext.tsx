2fa66acc908f7e06cb8d4747e9173723f8c0f4bc
Ajaya Bahadur K C <kshettryaj93@gmail.com>
2023-10-12 14:36:28 +1100

    Gitignore

--- pdf/components/chat/ChatContext.tsx
+++ pdf/components/chat/ChatContext.tsx
@@ -1,6 +1,8 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useRef, useState } from "react";
 import { useToast } from "../ui/use-toast";
 import { useMutation } from "@tanstack/react-query";
+import { trpc } from "@/app/_trpc/client";
+import { INFINITE_QUERY_LIMIT } from "@/config/infinite-query";
 
 type StreamResponse = {
   addMessage: () => void;
@@ -26,8 +28,12 @@ export const ChatContextProvider = ({ fileId, children }: Props) => {
   const [isLoading, setIsLoading] = useState(false);
   const { toast } = useToast();
 
+  const backupMessage = useRef("");
+
+  const utils = trpc.useContext();
+
   const { mutate: sendMessage } = useMutation({
-    mutationFn: async ({ message }: { message: String }) => {
+    mutationFn: async ({ message }: { message: string }) => {
       const response = await fetch("/api/message", {
         method: "POST",
         body: JSON.stringify({
@@ -40,6 +46,56 @@ export const ChatContextProvider = ({ fileId, children }: Props) => {
         throw new Error("Failed to send message");
       }
       return response.body;
+    },
+    onMutate: async ({ message }) => {
+      backupMessage.current = message;
+      setMessage("");
+
+      //Step 1
+      await utils.getFileMessages.cancel();
+
+      //step 2
+      const previousMessage = utils.getFileMessages.getInfiniteData();
+
+      //step 3
+      utils.getFileMessages.setInfiniteData(
+        { fileId, limit: INFINITE_QUERY_LIMIT },
+        (old) => {
+          if (!old) {
+            return {
+              pages: [],
+              pageParams: [],
+            };
+          }
+
+          let newPages = [...old.pages];
+
+          let latestPage = newPages[0]!;
+
+          latestPage.messages = [
+            {
+              createdAt: new Date().toISOString(),
+              id: crypto.randomUUID(),
+              text: message,
+              isUserMessage: true,
+            },
+            ...latestPage.messages,
+          ];
+
+          newPages[0] = latestPage;
+
+          return {
+            ...old,
+            pages: newPages,
+          };
+        }
+      );
+      setIsLoading(true);
+
+      return {
+        previousMessages:
+          previousMessage?.pages.flatMap((page) => page.messages) ?? [],
+      };
     },
   });
 
